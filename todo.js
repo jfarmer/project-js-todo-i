55c3d250203c316859cda3931679e1153b41a1cb
Jesse Farmer <jesse@20bits.com>
2020-02-08 12:44:14 -0800

    Add newTask and addTask functions
    - newTask will create a new task for the todo list
    - addTask will add a task to the given list

--- todo.js
+++ todo.js
@@ -1,17 +1,21 @@
-/*
-What types of objects do you need?
+function newTask(description) {
+  return {
+    description: description,
+  };
+};
 
-List out nouns and verbs involved in creating TODO lists.
-Decide which nouns and verbs you want/need to model.
-The nouns will be your objects and the values the functions.
+function addTask(list, task) {
+  list.push(task);
 
-Write simple functions that work on a few number of well-defined objects.
+  return list;
+}
 
-Keep the responsibilities separated as best you can:
+let todoList = [
+  newTask('Walk the dog'),
+  newTask('Bake a cake'),
+  newTask('Wash the car'),
+];
 
-1. Representing a real-life todo list as in-memory objects
-2. Manipulating those in-memory objects
-3. Reading and writing from the todos.txt file
-4. Displaying information to the user
-5. Rather user input and taking the appropriate actions
-*/
+addTask(todoList, newTask('Write a song'));
+
+console.log(todoList);
